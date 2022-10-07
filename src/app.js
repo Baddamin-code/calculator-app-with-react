5400d7e708bbd9227adcad7214c4bdc505ebf430
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-06 23:46:25 -0700

    create calculator buttons

---
+++ src/app.js
@@ -0,0 +1,33 @@
+
+
+function App() {
+  return (
+    <div className="calculator-grid">
+      <div className="output">
+        <div className="prev-operand"></div>
+        <div className="curr-operand"></div>
+      </div>
+      <button className="span-two">AC</button>
+      <button>DEL</button>
+      <button>/</button>
+      <button>1</button>
+      <button>2</button>
+      <button>3</button>
+      <button>*</button>
+      <button>4</button>
+      <button>5</button>
+      <button>6</button>
+      <button>+</button>
+      <button>7</button>
+      <button>8</button>
+      <button>9</button>
+      <button>7</button>
+      <button>-</button>
+      <button>.</button>
+      <button>0</button>
+      <button className="span-two">=</button>
+    </div>
+  );
+}
+
+export default App
