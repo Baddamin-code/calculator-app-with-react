4d4bcc10497018f84ae3bcd35eb9e21fad9e2cf0
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-07 05:27:00 -0700

    handleClick funtion

--- src/app.js
+++ src/app.js
@@ -4,6 +4,9 @@ import "./style.css";
 function App() {
   const [result, setResult] = useState("");
 
+  const handleClick = (e) => {
+    setResult(result.concat(e.target.name));
+  };
 
 
   return (
