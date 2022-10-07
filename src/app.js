0717398536254c1bbf1de57ea54da1bd062a73ce
Al-ameen Raji <alameenraji1@gmail.com>
2022-10-07 05:25:58 -0700

    add onclick attribute to buttons

--- src/app.js
+++ src/app.js
@@ -1,32 +1,38 @@
-import './style.css'
+import { useState } from "react";
+import "./style.css";
 
 function App() {
+
   return (
     <div className="calculator-grid">
       <div className="output">
-        <div className="prev-operand">563672832</div>
-        <div className="curr-operand">123785489</div>
+        <div className="prev-operand"></div>
+        <div className="curr-operand">{result}</div>
       </div>
-      <button className="span-two">AC</button>
-      <button>DEL</button>
-      <button>/</button>
-      <button>1</button>
-      <button>2</button>
-      <button>3</button>
-      <button>*</button>
-      <button>4</button>
-      <button>5</button>
-      <button>6</button>
-      <button>+</button>
-      <button>7</button>
-      <button>8</button>
-      <button>9</button>
-      <button>-</button>
-      <button>.</button>
-      <button>0</button>
-      <button className="span-two">=</button>
+      <button onClick={clear} className="span-two" id="clear">
+        AC
+      </button>
+      <button onClick={backspace} id="delete">
+        DEL
+      </button>
+      <button name="/" onClick={handleClick}>&divide;</button>
+      <button name="1" onClick={handleClick}>1</button>
+      <button name="2" onClick={handleClick}>2</button>
+      <button name="3" onClick={handleClick}>3</button>
+      <button name="*" onClick={handleClick}>*</button>
+      <button name="4" onClick={handleClick}>4</button>
+      <button name="5" onClick={handleClick}>5</button>
+      <button name="6" onClick={handleClick}>6</button>
+      <button name="+" onClick={handleClick}>+</button>
+      <button name="7" onClick={handleClick}>7</button>
+      <button name="8" onClick={handleClick}>8</button>
+      <button name="9" onClick={handleClick}>9</button>
+      <button name="-" onClick={handleClick}>-</button>
+      <button name="." onClick={handleClick}>.</button>
+      <button name="0" onClick={handleClick}>0</button>
+      <button name="=" className="span-two" onClick={calculate}>=</button>
     </div>
   );
 }
 
-export default App
+export default App;
